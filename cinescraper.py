3205cd61cebf6ff9c592951861136d295cc0a373
Domogo <dsatrapaa@gmail.com>
2019-05-24 11:19:29 +0200

    Get schedule for each movie

--- cinescraper.py
+++ cinescraper.py
@@ -7,8 +7,15 @@ html_page = urllib.urlopen("https://www.blitz-cinestar.hr/cinestar-rijeka-4dx")
 soup = BeautifulSoup(html_page, features="lxml")
 
 my_divs = soup.find_all("div", {"class": "movie_box"})
-titles = []
+movies = {}
 for div in my_divs:
     title = div.find("a", {"class": "movieItemTitle"}).text
-    print(title)
-    titles.append(title)
+    movies[title] = {}
+    schedule_times = div.find_all("a", {"class": "tips"})
+    times = []
+    for time in schedule_times:
+        times.append(time.text)
+    movies[title]["schedule"] = times
+
+for movie in movies:
+    print(movie + "\t" + str(movies[movie]['schedule']))
